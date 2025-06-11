3bbb8083cf3a66ec3f20f2de590ef060975d3bc0
Heng <loemheng123kh@gmail.com>
2025-06-11 13:10:24 +0700

    Update channels.php

--- Luci-app/routes/channels.php
+++ Luci-app/routes/channels.php
@@ -6,9 +6,6 @@ Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
-Broadcast::channel('App.Models.Admin.{id}', function ($admin, $id) {
-    return (int) $admin->id === (int) $id;
-});
 
 
 
